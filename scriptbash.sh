7f62a3f1d62eda15874b75a0b845376e17d1517f
Bakary Seydou Diaby <bakaryseydou@gmail.com>
2020-12-27 08:56:29 +0100

    changes done

--- scriptbash.sh
+++ scriptbash.sh
@@ -19,17 +19,17 @@ taper une option parmis :
     - start : demarrer les services
     - stop : arreter les services
     - infos : obtenir les IP
-    - add-user : ajouter un utilisateur
+    - add-user: ajouter un utilisateur sur un serveur web à preciser
     - lister-container : liste par ID,NAME et STATUS uniquement
-    - connexion-master : ssh avec exec sur master ansible
+    - connexion-VM : ssh avec exec sur les VMs
     - le reste : unknown
     ==> : 
 "
 
 read option
-
+# ==> c'est avec read qu'on nous demande de taper un des options citées
 case $option in
- start)
+    start)
     echo -n "demarrage des containers"
     docker-compose start
     ;;
@@ -44,29 +44,43 @@ case $option in
     docker-compose ps --format "table {{.ID}}\t{{.Names}}\t{{.Status}}"
     ;;
      
-    infos)
+    infos-VM)
     echo -n "==> ip des containers :
     "
-
     docker network inspect -f \
     '{{json .Containers}}' $(docker network ls | grep docker_ansible_default | awk '{print $1}') | \
     jq '.[] | .Name + " : ==> " + .IPv4Address'
     ;;
 
     add-user)
-    echo -n "ajout utilisateur sur le master ansible : "
+    echo -n " Entrer le nom de l'utilisateur (devops) : 
+    "
     read nom
+    docker network inspect -f \
+    '{{json .Containers}}' $(docker network ls | grep docker_ansible_default | awk '{print $1}') | \
+    jq '.[] | .Name + " : ==> " + .IPv4Address'
+    sleep 3
+    echo -n " Entrer le nom de la VM : "
+    read VM
     echo "    => création de l'utilisateur  ${nom}"
-    docker exec -ti docker_ansible_master_1 /bin/bash -c "useradd -m -p password $nom"
-    docker exec -ti docker_ansible_master_1 /bin/bash -c "echo '${nom}   ALL=(ALL) NOPASSWD: ALL'>>/etc/sudoers"
+    docker exec $VM /bin/bash -c "useradd -m -p password $nom"
+    docker exec -ti $VM /bin/bash -c "echo '${nom}   ALL=(ALL) NOPASSWD: ALL' >>/etc/sudoers"
     ;;
-
-    connexion-master)
-    echo -n " ==> connexion sur le master"
-    docker exec -ti docker_ansible_master_1 /bin/bash
+                        # devops  ALL=(ALL) NOPASSWD: ALL
+    connexion-VM)
+    docker network inspect -f \
+    '{{json .Containers}}' $(docker network ls | grep docker_ansible_default | awk '{print $1}') | \
+    jq '.[] | .Name + " : ==> " + .IPv4Address'
+    sleep 3
+    echo -n " ==> connexion sur le serveur
+    "
+    echo -n " Entrer le nom de la VM : "
+    read VM
+    docker exec -ti $VM /bin/bash
     ;;
 
     *)
-    echo -n "unknown"
+    echo -n "unknown
+    "
     ;;
 esac
